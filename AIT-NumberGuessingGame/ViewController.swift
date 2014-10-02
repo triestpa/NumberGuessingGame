2c86f85e0abe01a06e0bbf972321da7c5c8c4961
triestpa <triestpa@grinnell.edu>
2014-10-02 23:13:31 +0200

    add animations

--- AIT-NumberGuessingGame/ViewController.swift
+++ AIT-NumberGuessingGame/ViewController.swift
@@ -20,14 +20,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var playAgainButton: UIButton!
     
-    var targetValue: Int = Int(arc4random_uniform(10))
+    var targetValue: Int = 5//Int(arc4random_uniform(10))
     var currentValue: Int = 5
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    //  gradientInit()
+        self.backgroundView.backgroundColor = UIColor.redColor()
         sliderNumberView.text = "5"
-        gradientInit()
         playAgainButton.enabled = false
         
     }
@@ -37,21 +38,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    /* Use to init gradient, decided not to use it
     func gradientInit() {
         let darkBlue: CGColorRef = UIColor(red: 66.0/255.0, green: 113.0/255.0, blue: 242.0/255.0, alpha: 1).CGColor
-        
         let lightBlue: CGColorRef = UIColor(red: 129.0/255.0, green: 242.0/255.0, blue: 253.0/255.0, alpha: 1).CGColor
-        
         let arrayColors: NSArray = [lightBlue, darkBlue]
         
         let gl: CAGradientLayer = CAGradientLayer()
         gl.colors = arrayColors
-        gl.locations = [ 0.0, 1.0]
+        gl.locations = [0.0, 0.5]
         gl.frame = backgroundView.bounds
         
         backgroundView.layer.insertSublayer(gl, atIndex: 0)
     }
-    
+        */
+
     @IBAction func numberSliderAction(sender: AnyObject) {
         currentValue = Int(numberSlider.value * 10)
         var valueString = NSString(format: "%d", currentValue)
@@ -67,15 +68,30 @@ class ViewController: UIViewController {
         }
         else {
             feedbackText.text = "You Win!"
-            playAgainButton.alpha = 1
+            //playAgainButton.alpha = 1
             playAgainButton.enabled = true
+            
+            playAgainButton.alpha = 0.0
+            UIView.animateWithDuration(2, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: .CurveEaseIn, animations: {
+                self.backgroundView.backgroundColor = UIColor.blueColor()
+                self.playAgainButton.alpha = 1.0
+                self.playAgainButton.center = CGPoint(x: 10, y: 100)
+                }, completion: nil)
         }
     }
 
     @IBAction func playAgainButtonAction(sender: AnyObject) {
         targetValue = Int(arc4random_uniform(10))
         feedbackText.text = "Make A Guess!"
-        playAgainButton.alpha = 0
+        
+        
+        UIView.animateWithDuration(0.8, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8,
+            options: .CurveEaseOut
+            , animations: {
+            self.playAgainButton.alpha = 0.0
+            self.backgroundView.backgroundColor = UIColor.redColor()
+            }, completion: nil)
+        
         playAgainButton.enabled = false
     }
 }
